7c8e55c6ceedcc08438975e04316dedd77e65e80
Kotamoss <storyofko@gmail.com>
2018-12-10 11:19:50 -0500

    added variable 'isGreen' to view controller document.

--- hello-color/ViewController.swift
+++ hello-color/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isGreen = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func changeColor(_ sender: Any) {
+        if isGreen {
+            
+        }
     }
     
 }
